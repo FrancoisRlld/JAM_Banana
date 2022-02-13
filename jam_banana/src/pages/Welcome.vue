bb5d1f68bd455228fa06c5e96890cab405604dd7
Viu09 <silviu.zaino@epitech.eu>
2022-02-13 20:07:13 +0100

    {Add} Welcome page (home) nearly complete

--- jam_banana/src/pages/Welcome.vue
+++ jam_banana/src/pages/Welcome.vue
@@ -1,16 +1,159 @@
 <template>
-    <!-- Oui, les couleurs sont dégueulasses. Si vous allez sur la doc Quasar, dans la section <Style & Identity>, vous pourrez voir TOUTES les possibilités et changer a votre guise -->
-    <div class="text-h5 text-bold text-green q-mt-lg" style="margin-bottom: 100px" align="center">
-        Welcome Page
-    </div>
+  <div style="text-align: center">
+      <h3 class="text-bold text-blue-grey-14">Hi !</h3>
+      <p class="text-subtitle2" style="text-size: 30px">Your Team -who is composed essentially of SuccessMaster- which you a great Future !</p>
+      <p class="text-subtitle1" style="margin-top: 50px">If you're reading this, CONGRATS !</p>
+      <p style="margin-bottom: 70px">You have accomplished the first Step to become a SuccessMaster !</p>
+      <p class="text-h6 text-bold">Now, let's go to the Future !</p>
+      <p>If you want to become as successfull as the SuccessMasters, scroll down to learn the Sacred Secrets of the SuccessMasters</p>
+  </div>
 
-    <div>
-        Il faudrait mettre les photos de ceux qui ont fait partie du groupe de la Jam et nous mettre une légende du style SuccessMaster.
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-between">
+    <q-parallax
+      src="https://cdn.quasar.dev/img/parallax2.jpg">
+    >
+      <h5 class="text-white">How to Become a SuccessMaster</h5>
+    </q-parallax>
     </div>
-    <div>
-        J'aimerai une explication plus sérieuse que la mienne dans le bouton "The Game". Si possible, rajoutez le trailer en dessous.
-    </div>
-    <div>
-        Si vous avez des autres idées, faites vous plaisr, mais dans la limite du raisonnable.
+  </div>
+
+  <div style="text-align: center">
+        <h3 class="q-mb-lg">Who are we ?</h3>
+        <p class="text-h6">Obviously, the Grand SuccessMasters !</p>
+        <p class="text-subtitle1" style="margin-bottom: 75px">Yes, we know, you want more and more infos</p>
+        <h5>The SuccessMasters are composed of :</h5>
+  </div>
+
+  <div class="align-items">
+
+      <div class="q-pa-md">
+        <div class="row justify-center q-gutter-sm">
+          <q-intersection
+            transition="scale"
+            class="example-item"
+          >
+            <q-card class="q-ma-sm">
+              <img src="https://cdn.quasar.dev/img/mountains.jpg">
+              <q-card-section>
+                <div class="row">
+                  <div class="col" style="margin-left: 50px">
+                    <div class="text-subtitle2">Head of the SuccessMasters</div>
+                    <div class="text-h6">Silviu Zaino</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Level</div>
+                    <div class="text-h6">199ème Cercle</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Member since</div>
+                    <div class="text-h6">Year 800</div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="q-ma-sm q-mt-lg">
+              <img src="https://cdn.quasar.dev/img/mountains.jpg">
+              <q-card-section>
+                <div class="row">
+                  <div class="col" style="margin-left: 50px">
+                    <div class="text-subtitle2">Second of the SuccessMasters</div>
+                    <div class="text-h6">François Rolland</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Level</div>
+                    <div class="text-h6">150ème Cercle</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Member since</div>
+                    <div class="text-h6">Year 1950</div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="q-ma-sm q-mt-lg">
+              <img src="https://cdn.quasar.dev/img/mountains.jpg">
+              <q-card-section>
+                <div class="row">
+                  <div class="col" style="margin-left: 50px">
+                    <div class="text-subtitle2">Archbishop of the SuccessMasters</div>
+                    <div class="text-h6">Floriane Mantey</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Level</div>
+                    <div class="text-h6">100ème Cercle</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Member since</div>
+                    <div class="text-h6">Year 1970</div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="q-ma-sm q-mt-lg">
+              <img src="https://cdn.quasar.dev/img/mountains.jpg">
+              <q-card-section>
+                <div class="row">
+                  <div class="col" style="margin-left: 50px">
+                    <div class="text-subtitle2">High Priest of the SuccessMasters</div>
+                    <div class="text-h6">Benjamin Messmer</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Level</div>
+                    <div class="text-h6">90ème Cercle</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Member since</div>
+                    <div class="text-h6">Year 1990</div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="q-ma-sm q-mt-lg">
+              <img src="https://cdn.quasar.dev/img/mountains.jpg">
+              <q-card-section>
+                <div class="row">
+                  <div class="col" style="margin-left: 50px">
+                    <div class="text-subtitle2">Priest of the SuccessMasters</div>
+                    <div class="text-h6">Victorien Conde</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Level</div>
+                    <div class="text-h6">80ème Cercle</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Member since</div>
+                    <div class="text-h6">Year 1990</div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="q-ma-sm q-mt-lg">
+              <img src="https://cdn.quasar.dev/img/mountains.jpg">
+              <q-card-section>
+                <div class="row">
+                  <div class="col" style="margin-left: 50px">
+                    <div class="text-subtitle2">New SuccessMaster</div>
+                    <div class="text-h6">Thomas Dehennault</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Level</div>
+                    <div class="text-h6">50ème Cercle</div>
+                  </div>
+                  <div class="col" style="margin-left: 70px">
+                    <div class="text-subtitle2">Member since</div>
+                    <div class="text-h6">Year 2005</div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </q-intersection>
+        </div>
+      </div>
     </div>
 </template>
