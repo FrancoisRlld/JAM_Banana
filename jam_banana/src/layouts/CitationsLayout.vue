f9fe6eedf1b1377c96801279e73be0c8678bc91a
Floriane Mantey <floriane.mantey@epitech.eu>
2022-02-13 18:05:25 +0100

    {ADD} Citation page & generation

--- jam_banana/src/layouts/CitationsLayout.vue
+++ jam_banana/src/layouts/CitationsLayout.vue
@@ -1,13 +1,22 @@
 <template>
-    <div style="margin-bottom: 50px" class="q-mt-lg q-mr-lg" align="right">
+    <div class="q-mt-lg q-mr-lg" align="right">
         <!--  Le bouton Return est entièrement fonctionnel -->
         <q-btn color="red" @click="prevPage">
             RETURN
         </q-btn>
     </div>
-    <h5 style="text-align: center">Ici, des citations à afficher.</h5>
-    <div align="center" class="text-body2">
-        J'aimerai bien 20 citations minimum, placées dans un tableau en behind et qui appelle en random une citation a chaque fois qu'on appuie sur le bouton.
+
+    <div align="center">
+    <q-card class="my-card">
+      <q-card-section>
+        <h3 class="text">{{quote}}</h3>
+        <img v-bind:src="selectedImage" class="img"/>
+      </q-card-section>
+    </q-card>
+
+      <q-btn color="green" v-on:click="newQuote">
+        <h3>Génerer une citation</h3>
+      </q-btn>
     </div>
 
 </template>
@@ -19,7 +28,6 @@ import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'CitationsLayout',
-
   setup () {
     const router = useRouter()
     return {
@@ -27,6 +35,68 @@ export default defineComponent({
         return router.push({ path: '/' })
       }
     }
+  },
+
+  data () {
+    return {
+      el: '#app',
+      random: 0,
+      quote: 'Pouet',
+      quotes: ['Une raison pour le succès vaut bien la raison du succès.',
+        'Le succès de l\'insuccès est un succès.',
+        'Le succès de l\'insuccès est un succès.',
+        'Le succès est un menteur. Le menteur aime le succès.',
+        'Le mérite envie le succès, et le succès se prend pour le mérite.',
+        'Les succès produisent les succès, comme l\'argent produit l\'argent.',
+        'Plaire n\'est pour lui qu\'un moyen de succès ; tandis que pour elle, c\'est le succès lui-même.',
+        'Les auteurs qui pillent sans vergogne leurs aînés ont parfois du talent ; ils “volent” de succès en succès.',
+        'Lorsqu\'un film a du succès, c\'est une affaire. Lorsqu\'il n\'a pas de succès, c\'est de l\'art.',
+        'La raison pour laquelle un succès précoce peut désorienter un écrivain, c\'est que ce succès peut le pousser à surproduire.',
+        'La chance fait le succès, et le succès fait le mérite.',
+        'Ce n\'est pas parce que c\'est un succès qu\'un film est bon et ce n\'est pas parce qu\'un film est bon que c\'est un succès.',
+        'Un homme qui a du succès est un homme qui gagne plus d\'argent que sa femme n\'en dépense. Une femme qui a du succès est celle qui trouve un tel homme.',
+        'Le succès commercial, le seul qui vaille. Les succès d\'estime ne conduisent jamais leur bénéficiaire qu\'aux épinards sans beurre.',
+        'Le côté honteux de l\'écriture c\'est lorsque le succès ne vient pas la couronner. Le succès n\'est rien que la permission de continuer.',
+        'Ce n\'est pas le succès qui rend heureux, vraiment pas. Le succès, c\'est faire ce qui vous rend heureux, bien travailler et avoir une vie riche.',
+        'Là où il y a du monde, les Parisiens viennent en foule. Ce qui fait le succès d\'un endroit, c\'est qu\'il a du succès...',
+        'De nombreux hommes doivent leur succès à leur première femme et leur seconde femme à leur succès.',
+        'La véritable épreuve de l\'amitié, c\'est le succès : car le malheur ne réclame que du secours et ne risque que la résistance de l\'avarice ; tandis que le succès voudrait de l\'affection et ne rencontre que l\'envie.',
+        'Prends garde au lendemain du succès !'
+      ],
+      images: ['http://localhost:8080/img/1.e174be91.jpg',
+        'http://localhost:8080/img/2.1ebe9212.jpg',
+        'http://localhost:8080/img/3.c15eb9c8.jpg',
+        'http://localhost:8080/img/4.cc0a63d4.jpg',
+        'http://localhost:8080/img/5.5bc41d87.jpg',
+        'http://localhost:8080/img/6.c99594c1.jpg',
+        'http://localhost:8080/img/7.74872cbe.jpg',
+        'http://localhost:8080/img/8.088110e5.jpg',
+        'http://localhost:8080/img/9.3c10261f.jpg',
+        'http://localhost:8080/img/10.4d1cf8ba.jpg',
+        'http://localhost:8080/img/11.e672ba73.jpg'],
+      selectedImage: ''
+    }
+  },
+  methods: {
+    newQuote: function () {
+      this.random = Math.floor(Math.random() * 20) + 0
+      this.quote = this.quotes[this.random]
+      const idx = Math.floor(Math.random() * 11)
+      this.selectedImage = this.images[idx]
+    }
   }
 })
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  height: 650px
+  max-width: 1000px
+  margin-bottom: 50px
+
+.img
+  position:fixed
+  bottom:0px
+  height: 250px
+</style>
